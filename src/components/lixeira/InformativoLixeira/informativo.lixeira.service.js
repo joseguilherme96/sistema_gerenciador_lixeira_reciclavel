e28a0751ede140f886176756d1e3f1c5af662d8e
José GUilherme <jose.guilherme96@outlook.com.br>
2025-03-30 17:43:28 -0300

    Retirado importação não utilizada

--- src/components/lixeira/InformativoLixeira/informativo.lixeira.service.js
+++ src/components/lixeira/InformativoLixeira/informativo.lixeira.service.js
@@ -1,6 +1,5 @@
 
 import { ref } from 'vue'
-import { lixeiraSelecionada } from '../lixeira.service.js'
 
 
 const baseUrl = "http://localhost:3000";
@@ -11,9 +10,9 @@ export const informativoLixeira = ref([])
 export async function getInformativoLixeiraPorPontoLixoId(ponto_lixo_id) {
 
     fetch(`${apiInformativoLixeira}?pontoLixoId=${ponto_lixo_id}`)
-        .then(res => res.json())
-        .then(res => {
-            
+    .then(res => res.json())
+    .then(res => {
+
             informativoLixeira.value = res;
             informativoLixeira.value.sort((a, b) => b.idInformativoLixeira - a.idInformativoLixeira)
 
