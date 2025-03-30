5b7c947bda5e50b562fa1f56ab71f3c16bf14dfe
José GUilherme <jose.guilherme96@outlook.com.br>
2025-03-30 17:44:19 -0300

    Retirado importação não utilizada

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import MenuNavegacao from './components/template/MenuNavegacao.vue'
 
 
