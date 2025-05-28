1ede54c13b05aacefca1663360891af523b5d658
José GUilherme <jose.guilherme96@outlook.com.br>
2025-05-28 18:20:00 -0300

    Criado componente VChipLixeiraEstaAberta com gerenciador de estado

---
+++ src/components/lixeira/VChips/VChipLixeiraEstaAberta.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import { mdiDeleteEmpty, mdiDelete } from '@mdi/js';
+import { storeToRefs } from 'pinia'
+import { useLixeiraStore } from '@/stores/lixeira'
+const lixeiraStore = useLixeiraStore()
+const { lixeira } = storeToRefs(lixeiraStore)
+
+</script>
+<template>
+    <v-chip :color="lixeira.esta_aberta == 'Aberta' ? 'brown' : 'green'">
+        <svg-icon type="mdi" :path="lixeira.esta_aberta == 'Aberta' ? mdiDeleteEmpty : mdiDelete"></svg-icon>
+        {{
+            lixeira.esta_aberta }}</v-chip>
+</template>
